c37bd32291bd62967894e7a600cb1c86f3b9fdc8
Wido den Hollander <wido@widodh.nl>
2016-01-29 12:40:14 +0100

    Clean up before trying build

--- scripts/packaging/clone-source.sh
+++ scripts/packaging/clone-source.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-git clone https://github.com/apache/cloudstack.git
+DEST="cloudstack"
+
+if [ -d "$DEST" ]; then
+    rm -r $DEST
+fi
+
+git clone https://github.com/apache/cloudstack.git $DEST
 
 if [ ! -z "$1" ]; then
     if [ "$1" != "master" ]; then
