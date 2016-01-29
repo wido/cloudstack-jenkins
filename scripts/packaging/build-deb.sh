01e22e464615d150c2a372f5adfb23cf711ee9c4
Wido den Hollander <wido@widodh.nl>
2016-01-29 12:23:06 +0100

    Do not switch to directory twice

--- scripts/packaging/build-deb.sh
+++ scripts/packaging/build-deb.sh
@@ -6,8 +6,6 @@ BRANCH="master"
 
 `dirname $0`/clone-source.sh $BRANCH
 
-cd cloudstack
-
 apt-get update
 apt-get install -y dpkg-dev debhelper openjdk-7-jdk genisoimage python-mysqldb maven python
 
